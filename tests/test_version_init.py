6d4ff2a3735e6cf91c9c5076a68f02b8f3933203
Martin Mudroch <martinkamil.mudroch@gmail.com>
2024-02-13 21:42:38 +0100

    accept prerelease and build in Version constructor (prerelease and build are not comparable yet)

--- tests/test_version_init.py
+++ tests/test_version_init.py
@@ -16,19 +16,32 @@ class VersionInitTestCase(unittest.TestCase):
         self.assertEqual(from_version.patch, new_version.patch)
         self.assertEqual(from_version.fix, new_version.fix)
 
-    def test_from_str_without_fix(self):
+    def test_from_str_mandatory_parts(self):
         version = Version(version='4.2.0')
         self.assertEqual(version.major, 4)
         self.assertEqual(version.minor, 2)
         self.assertEqual(version.patch, 0)
         self.assertEqual(version.fix, 0)
+        self.assertIsNone(version.prerelease)
+        self.assertIsNone(version.build)
 
     def test_from_str_with_fix(self):
-        version = Version(version='4.2.0.9')
+        version = Version(version='4.2.0.9-alpha.9+989898')
         self.assertEqual(version.major, 4)
         self.assertEqual(version.minor, 2)
         self.assertEqual(version.patch, 0)
         self.assertEqual(version.fix, 9)
+        self.assertEqual(version.prerelease, 'alpha.9')
+        self.assertEqual(version.build, '989898')
+
+    def test_from_str_without_fix(self):
+        version = Version(version='4.2.0-alpha.9+989898')
+        self.assertEqual(version.major, 4)
+        self.assertEqual(version.minor, 2)
+        self.assertEqual(version.patch, 0)
+        self.assertEqual(version.fix, 0)
+        self.assertEqual(version.prerelease, 'alpha.9')
+        self.assertEqual(version.build, '989898')
 
     def test_from_str_wrong_format(self):
         for invalid_v in ['1.2.a', '1.2.5.a', '1.2.3.5.6', '1.2', '1.02.3']:
@@ -37,19 +50,23 @@ class VersionInitTestCase(unittest.TestCase):
     def test_from_str_wrong_type(self):
         self.assertRaises(InvalidVersionError, Version, version=99)
 
-    def test_set_major_minor_patch(self):
+    def test_set_mandatory_parts(self):
         version = Version(major=4, minor='2', patch='0')
         self.assertEqual(version.major, 4)
         self.assertEqual(version.minor, 2)
         self.assertEqual(version.patch, 0)
         self.assertEqual(version.fix, 0)
+        self.assertIsNone(version.prerelease)
+        self.assertIsNone(version.build)
 
-    def test_set_major_minor_patch_fix(self):
-        version = Version(major=4, minor='2', patch='0', fix=9)
+    def test_set_kw(self):
+        version = Version(major=4, minor='2', patch='0', fix=9, prerelease='alpha.5', build='989529')
         self.assertEqual(version.major, 4)
         self.assertEqual(version.minor, 2)
         self.assertEqual(version.patch, 0)
         self.assertEqual(version.fix, 9)
+        self.assertEqual(version.prerelease, 'alpha.5')
+        self.assertEqual(version.build, '989529')
 
     def test_version_part_negative_int(self):
         self.assertRaises(
@@ -64,12 +81,14 @@ class VersionInitTestCase(unittest.TestCase):
         )
 
     def test_iterator(self):
-        version = Version(major=4, minor='2', patch='0')
-        self.assertEqual({'major': 4, 'minor': 2, 'patch': 0, 'fix': 0}, dict(version))
+        version = Version(major=4, minor='2', patch='0', prerelease='alpha', build='123')
+        self.assertEqual({'major': 4, 'minor': 2, 'patch': 0, 'fix': 0, 'prerelease': 'alpha', 'build': '123'}, dict(version))
 
     def test_get_item(self):
-        version = Version('4.2.0')
+        version = Version('4.2.0.7-alpha+56')
         self.assertEqual(4, version['major'])
         self.assertEqual(2, version['minor'])
         self.assertEqual(0, version['patch'])
-        self.assertEqual(0, version['fix'])
+        self.assertEqual(7, version['fix'])
+        self.assertEqual('alpha', version['prerelease'])
+        self.assertEqual('56', version['build'])
