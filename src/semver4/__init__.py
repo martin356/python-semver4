d537fe2f91ae2ca34f95c75d78087e8259f31372
Martin Mudroch <martinkamil.mudroch@gmail.com>
2024-02-15 23:16:33 +0100

    Fix version parsing

--- src/semver4/__init__.py
+++ src/semver4/__init__.py
@@ -9,16 +9,12 @@ from semver4.errors import (
 )
 
 
-__version__ = '0.0.1-beta.2'
+__version__ = '0.0.1-beta.3'
 
 
 class Version:
 
     _valid_version_regex = '^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:\.(?P<fix>0|[1-9]\d*))?(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
-
-    @classmethod
-    def valid_version_regex(cls):
-        return f
 
     @staticmethod
     def validate(version, raise_err=False):
