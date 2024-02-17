be2e37ecb88620d823a0aca25c36f7bada339d3c
Martin Mudroch <martinkamil.mudroch@gmail.com>
2024-02-17 18:10:04 +0100

    refactor test main module

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,16 +3,18 @@ import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1] / 'src'))
 
 import unittest
-from test_version_init import Version4InitTestCase
-from test_version_comparison import BaseVersionCompareTestCase
-from test_version_validation import Version4ValidationTestCase
+import importlib
 
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite([
-        unittest.makeSuite(Version4InitTestCase),
-        unittest.makeSuite(BaseVersionCompareTestCase),
-        unittest.makeSuite(Version4ValidationTestCase)
-    ])
+    excludes = []
+
+    is_test_case = lambda v: isinstance(v, type) and issubclass(v, unittest.TestCase)
+
+    py_items = pathlib.Path(__file__).resolve().parent.glob('test_*.py')
+    testmodules = [importlib.import_module(str(i.stem)) for i in py_items if i.is_file()]
+    testcases = [a for m in testmodules for a in [getattr(m, i) for i in dir(m)] if is_test_case(a)]
+
+    suite = unittest.TestSuite([unittest.makeSuite(t) for t in set(testcases)])
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
